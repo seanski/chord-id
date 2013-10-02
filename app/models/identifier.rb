26ecbd8b92cbabbb182380b407dd00a844b74a7c
Gregory McCubbin <gwmccubbin@gmail.com>
2013-10-02 12:49:21 -0500

    Refactored business logic

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -1,38 +1,34 @@
 class Identifier
-  extend ActiveModel::Naming
+  extend  ActiveModel::Naming
   include ActiveModel::Conversion
 
-  attr_accessor :notes, :base_note
+  attr_accessor :chord_scale, :base_note
 
   def persisted?
     false
   end  
 
-  def identify(notes)
-    notes.delete_if { |n| n.blank? }
-    self.base_note = notes.shift
-    self.notes = self.class.notes.rotate(self.class.notes.index(self.base_note))
+  def identify(chord_tones)
+    chord_tones.delete_if { |n| n.blank? } # if n == base_note
+    self.base_note   = chord_tones.shift # not permanetnly removed, only if .shift!
+    base_note_index  = Identifier.chromatic_scale.index(self.base_note)
+    self.chord_scale = Identifier.chromatic_scale.rotate(base_note_index)
 
-    selected_indexes = notes.map { |n| self.notes.index(n) }
+    chord_tones_indexes = chord_tones.map { |n| self.chord_scale.index(n) } 
+    chord_tones_indexes.sort!
 
-    selected_indexes = selected_indexes.sort
-
-    offset_index = self.class.offsets.index(selected_indexes)
-    chord = "#{self.base_note} #{self.class.qualities[offset_index]}"
+    chord_definitions_index = Identifier.chord_definitions.index(chord_tones_indexes)
+    chord = "#{self.base_note} #{Identifier.chord_qualities[chord_definitions_index]}"
     chord
   end
 
   class << self
     
-    def notes
+    def chromatic_scale
       %w(C C#/Db D D#/Eb E F F#/Gb G G#/Ab A A#/Bb B)
     end
 
-    def qualities
-      %w(Major Minor Diminished Augmented Major-7th Minor-7th Domniant-7th Half-Diminished Diminished-7th)
-    end
-
-    def offsets
+    def chord_definitions
       [
         [4, 7],
         [3, 7],
@@ -46,6 +42,8 @@ class Identifier
       ]
     end
 
+    def chord_qualities
+      %w(Major Minor Diminished Augmented Major-7th Minor-7th Domniant-7th Half-Diminished Diminished-7th)
+    end
   end
-
 end
