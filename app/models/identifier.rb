f682492ff378314b1ee4635cbfc4138500fa2519
Gregory McCubbin <gwmccubbin@gmail.com>
2013-10-03 14:32:55 -0500

    Created chords hash & added new definitions

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -17,9 +17,12 @@ class Identifier
     chord_tones_indexes = chord_tones.map { |n| self.chord_scale.index(n) } 
     chord_tones_indexes.sort!
 
-    chord_definitions_index = Identifier.chord_definitions.index(chord_tones_indexes)
-    chord = "#{self.base_note} #{Identifier.chord_qualities[chord_definitions_index]}"
-    chord
+    if Identifier.chord_definitions[chord_tones_indexes]
+      chord_name = "#{self.base_note} #{Identifier.chord_definitions[chord_tones_indexes]}"
+      chord_name
+    else
+      false
+    end  
   end
 
   class << self
@@ -29,21 +32,43 @@ class Identifier
     end
 
     def chord_definitions
-      [
-        [4, 7],
-        [3, 7],
-        [3, 6],
-        [4, 8],
-        [4, 7, 11],
-        [3, 7, 10],
-        [4, 7, 10],
-        [3, 6, 10],
-        [3, 6, 9]
-      ]
-    end
-
-    def chord_qualities
-      %w(Major Minor Diminished Augmented Major-7th Minor-7th Domniant-7th Half-Diminished Diminished-7th)
+      { [7]        => "5 chord or \"Power Chord\"",
+        [4]        => "Implied Major",
+        [3]        => "Implied Minor",
+        [10]       => "Implied 7th",
+        [4,7]      => "Major",
+        [3,7]      => "Minor",
+        [4,8]      => "Augmented",
+        [3,6]      => "Diminished",
+        [2,7]      => "Sus 2",
+        [5,7]      => "Sus 4",
+        [4,7,11]   => "Major 7th",
+        [4,7,10]   => "Dominant 7th",
+        [3,7,10]   => "Minor 7th",
+        [3,6,10]   => "Half Diminished",
+        [3,7,9]    => "Diminished 7th",
+        [4,7,9]    => "Major 6th",
+        [3,7,9]    => "Minor 6th",
+        [2,4,7]    => "Major Add 9",
+        [2,3,7]    => "Minor Add 9",
+        [5,7,10]   => "7 sus",
+        [2,4,7,11] => "Major 9th",
+        [2,4,7,10] => "Dominant 9th",
+        [2,3,7,10] => "Minor 9th",
+        [4,7,9,11] => "Major 13th",
+        [4,7,9,10] => "Dominant 13th",
+        [2,4,7,9]  => "6/9",
+        [3,5,7,10] => "Minor 11th",
+        [4,6,7,11] => "Major 7th #11",
+        [4,6,7,10] => "Dominant #11",
+        [1,4,7,10] => "Dominant 7th b9",
+        [3,4,7,10] => "Dominant 7th  #9",
+        [2,4,6,10] => "Dominant 9th b5",
+        [2,4,8,10] => "Dominant 9th #5",
+        [4,7,8,10] => "Dominant 7th b13",
+        [1,3,6,10] => "Half Diminished b9",
+        [2,3,6,10] => "Half Diminished 9th",
+      }
     end
   end
 end
